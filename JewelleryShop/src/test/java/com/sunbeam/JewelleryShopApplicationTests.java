6205297927ca228605b3ea2a4898d428ab023a35
w3-80717 <chavan1609@gmail.com>
2023-12-28 23:05:28 +0530

    Jewellery Project Begin

---
+++ JewelleryShop/src/test/java/com/sunbeam/JewelleryShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunbeam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JewelleryShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
